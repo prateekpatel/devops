4266970f6752b7e66acf326e80cd64b5e50e0c7d
Chelimala <raju.chelimala@hpe.com>
2016-12-02 15:23:53 +0530

    adding server folder

---
+++ src/gclassec/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    // Standard library packages
+    "net/http"
+
+    // Third party packages
+    "github.com/julienschmidt/httprouter"
+    "TestDb/controllers"
+)
+
+func main() {
+    // Instantiate a new router
+    r := httprouter.New()
+
+    // Get a InstController instance
+    uc := controllers.NewInstController()
+
+    // Get a instance resource
+    r.GET("/dbaas/list", uc.GetDetails)
+
+    // Fire up the server
+    http.ListenAndServe("localhost:9000", r)
+}
