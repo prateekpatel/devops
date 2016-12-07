157907bc13a17f675b7cb470d6891c8bbdc88f2c
Chelimala <raju.chelimala@hpe.com>
2016-12-07 12:28:05 +0530

    Adding controller and server

--- src/gclassec/controllers/controller.go
+++ src/gclassec/controllers/controller.go
@@ -1,18 +1,14 @@
 package controllers
 
-import(
+import (
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	_ "github.com/lib/pq"
-	_"github.com/go-sql-driver/mysql"
 	"net/http"
 	"github.com/julienschmidt/httprouter"
 	"encoding/json"
-	"fmt"
-	"time"
-	"github.com/jmoiron/sqlx"
 )
 type (
-    // InstController represents the controller for operating on the Instance resource
+    // InstController represents the controller for operating on the User resource
     InstController struct{}
 )
 
@@ -20,31 +16,131 @@ func NewInstController() *InstController {
     return &InstController{}
 }
 
-type ec2_dynamic struct {
-	InsT_id string
-	EC2_cpu_util_max float64
-	EC2_cpu_util_min float64
-	EC2_cpu_util_avg float64
-	EC2_start_time time.Time
-	EC2_end_time time.Time
+type rds_dynamic struct{
+	DBInstanceIdentifier string `gorm:"column:DBInstanceIdentifier"`
+	CPUUtilization_max float64 `gorm:"column:CPUUtilization_max"`
+	DatabaseConnections_max float64 `gorm:"column:DatabaseConnections_max"`
 }
 
+type vw_rds struct {
+	API_Name string `gorm:"column:API_Name"`
+	Linux_On_Demand_cost string `gorm:"column:Linux_On_Demand_cost"`
+	Linux_Reserved_cost string `gorm:"column:Linux_Reserved_cost"`
+	Windows_On_Demand_cost string `gorm:"column:Windows_On_Demand_cost"`
+	Windows_Reserved_cost string `gorm:"column:Windows_Reserved_cost"`
+}
+
+/*type Ec2_dynamic struct {
+	Inst_id string `gorm:"column:inst_id"`
+	EC2_cpu_util_max float64 `gorm:"column:ec2_cpu_util_max"`
+	EC2_cpu_util_min float64 `gorm:"column:ec2_cpu_util_min"`
+	EC2_cpu_util_avg float64 `gorm:"column:ec2_cpu_util_avg"`
+	EC2_start_time string `sql:"type:date" gorm:"column:ec2_start_time"`
+	EC2_end_time string `sql:"type:date" gorm:"column:ec2_end_time"`
+}*/
+
+/*
+Get instance details
+*/
 func (uc InstController) GetDetails(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	dbObj := []rds_dynamic{}
 
-	db,err := sqlx.Connect("mysql", "root:root@tcp(110.110.110.170:3306)/cloud_assessment")
+    db, err := gorm.Open("mysql", "root:root@tcp(110.110.110.170:3306)/cloud_assessment")
+	println(db)
+	println(err)
 
-	fmt.Print(db)
+	db.SingularTable(true)
 
-	inst := ec2_dynamic{}
+	/*for _, v := range dbObj {
+		//fmt.Println("Id : ", v.Id)
+		//fmt.Println("Username : ", v.Name)
+		fmt.Println(v)
+	        _ = json.NewEncoder(w).Encode(db.Find(&dbObj))
+        }*/
 
-	rows, _ := db.Queryx("SELECT * FROM ec2_dynamic where ec2_cpu_util_max>90")
+	_ = json.NewEncoder(w).Encode(db.Find(&dbObj))
 
-	for rows.Next() {
-		rows.StructScan(&inst)
-	    _ = json.NewEncoder(w).Encode(inst)
-    	}
-
+	// Ping function checks the database connectivity
+	err = db.DB().Ping()
 	if err != nil {
-		println(err)
+		panic(err)
 	}
+	db.Close()
+}
+
+/*
+Get details of instance for the given id
+*/
+func (uc InstController) GetDetailsById(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	dbObj := []rds_dynamic{}
+
+	id := p.ByName("id")
+
+    db, err := gorm.Open("mysql", "root:root@tcp(110.110.110.170:3306)/cloud_assessment")
+	println(db)
+	println(err)
+
+	db.SingularTable(true)
+
+	_ = json.NewEncoder(w).Encode(db.Find(&dbObj, "DBInstanceIdentifier = ?", id))
+
+	// Ping function checks the database connectivity
+	err = db.DB().Ping()
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+}
+
+/*
+Get details of instances based on CPU Utilization and DB Connections
+*/
+func (uc InstController) GetDB(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	dbObj := []rds_dynamic{}
+
+	queryValue1 := r.URL.Query().Get("CPUUtilization_max")
+
+	queryValue2 := r.URL.Query().Get("DatabaseConnections_max")
+
+	println(queryValue1)
+	println(queryValue2)
+
+    db, err := gorm.Open("mysql", "root:root@tcp(110.110.110.170:3306)/cloud_assessment")
+	println(db)
+	println(err)
+
+	db.SingularTable(true)
+
+	//_ = json.NewEncoder(w).Encode(db.Find(&dbObj, "CPUUtilization_max<5 && DatabaseConnections_max=0"))
+
+	_ = json.NewEncoder(w).Encode(db.Where("CPUUtilization_max < ? AND DatabaseConnections_max = ?", queryValue1, queryValue2).Find(&dbObj))
+
+	// Ping function checks the database connectivity
+	err = db.DB().Ping()
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+}
+
+/*
+Get pricing depending on instance type
+*/
+func (uc InstController) GetPrice(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	dbObj := []vw_rds{}
+
+    db, err := gorm.Open("mysql", "root:root@tcp(110.110.110.170:3306)/cloud_assessment")
+	println(db)
+	println(err)
+
+	db.SingularTable(true)
+
+	_ = json.NewEncoder(w).Encode(db.Find(&dbObj))
+
+	// Ping function checks the database connectivity
+	err = db.DB().Ping()
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
 }
--- src/gclassec/sever/server.go
+++ src/gclassec/sever/server.go
@@ -6,7 +6,8 @@ import (
 
     // Third party packages
     "github.com/julienschmidt/httprouter"
-    "gclassec/controllers"
+
+    "CloudApp/controllers"
 )
 
 func main() {
@@ -19,6 +20,12 @@ func main() {
     // Get a instance resource
     r.GET("/dbaas/list", uc.GetDetails)
 
+    r.GET("/dbaas/list/:id", uc.GetDetailsById)
+
+    r.GET("/dbaas/get", uc.GetDB)
+
+    r.GET("/dbaas/pricing", uc.GetPrice)
+
     // Fire up the server
-    http.ListenAndServe("localhost:9000", r)
+    http.ListenAndServe("localhost:3000", r)
 }
