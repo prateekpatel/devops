feae5c946a3ce6bb86524636d54c0dce8610fce3
nilesh <nilesh>
2016-12-20 13:49:25 +0530

    New Folder

---
+++ src/gclassec/opClient/read1_json.go
@@ -0,0 +1,33 @@
+package opClient
+
+import (
+	"os"
+	"encoding/json"
+	"fmt"
+)
+
+type Configuration struct {
+    Dbtype    string
+    Dbname   string
+	Dbusername   string
+    Dbpassword   string
+	Dbhostname   string
+	Dbport   string
+}
+
+func Configurtion() Configuration{
+	//s,_:= os.Getwd()
+
+
+
+	file, _ := os.Open("E:\\Git\\goclassec\\goclassec\\conf1.json")
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	return configuration
+
+}
