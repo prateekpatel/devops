98294dee9c8b16d5d9bff19317705fba705a73a4
Priya M <priya.malamkar@atmecs.com>
2016-12-23 06:49:16 +0000

    Final Updation

--- src/gclassec/openstackInstance/openstack_struct.go
+++ src/gclassec/openstackInstance/openstack_struct.go
@@ -3,20 +3,20 @@ package openstackInstance
 type Instances struct{
 	//Id int 			`gorm:"column:id"`
 	Name string 		`gorm:"column:name"`
-	InstanceID string 	`gorm:"column:InstanceID"`
+	InstanceID string 	`gorm:"column:instance_id"`
 	Status string 		`gorm:"column:status"`
-	AvailabilityZone string `gorm:"column:AvailabilityZone"`
+	AvailabilityZone string `gorm:"column:availability_zone"`
 	CreationTime string 	`gorm:"column:CreationTime"`
-	Flavor string 		`gorm:"column:Flavor"`
-	FlavorID int 		`gorm:"column:FlavorID"`
-	RAM string 		`gorm:"column:RAM"`
-	VCPU string 		`gorm:"column:VCPU"`
-	Storage string 		`gorm:"column:Storage"`
-	IPAddress string	`sql:"type:decimal" gorm:"column:IPAddress"`
-	SecurityGroup string 	`gorm:"column:SecurityGroup"`
-	KeyPairName string 	`gorm:"column:KeyPairName"`
-	ImageName string 	`gorm:"column:ImageName"`
-	Volumes string 		`gorm:"column:Volumes"`
-	InsertionDate string 	`sql:"type:date" gorm:"column:InsertionDate"`
+	Flavor string 		`gorm:"column:flavor"`
+	FlavorID int 		`gorm:"column:flavor_id"`
+	RAM string 		`gorm:"column:ram"`
+	VCPU string 		`gorm:"column:vcpu"`
+	Storage string 		`gorm:"column:storage"`
+	IPAddress string	`sql:"type:decimal" gorm:"column:ip_address"`
+	SecurityGroup string 	`gorm:"column:security_group"`
+	KeyPairName string 	`gorm:"column:keyPair_name"`
+	ImageName string 	`gorm:"column:image_name"`
+	Volumes string 		`gorm:"column:volumes"`
+	InsertionDate string 	`sql:"type:date" gorm:"column:insertion_date"`
 
 }
